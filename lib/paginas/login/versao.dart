3264c10a48bec0956a32fa9ce552d74ca1e0e7d7
Prof Catalunha <catalunha.mj@gmail.com>
2020-02-05 09:32:07 -0300

    atualizando versao

--- lib/paginas/login/versao.dart
+++ lib/paginas/login/versao.dart
@@ -12,7 +12,7 @@ class Versao extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           ListTile(
-            title: Recursos.instance.plataforma == 'android' ? Text("Versão Android: 1.0.0") : Text("Versão Chrome: 1.0.0 build: 20200205"),
+            title: Recursos.instance.plataforma == 'android' ? Text("Versão Android: 1.0.0 (1)") : Text("Versão Chrome: 1.0.0 (1) build: 20200205"),
           ),
           // ListTile(
           //   title: Text("Suporte via WhatsApp pelo número +55 63 984495507"),
