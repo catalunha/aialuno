e110d9c88effddfc098e95d353ccf2eb9507eb51
catalunha <catalunha@pop-os.localdomain>
2020-11-19 21:43:16 -0300

    rev

--- lib/uis/login/login_page_ds.dart
+++ lib/uis/login/login_page_ds.dart
@@ -19,12 +19,12 @@ class LoginPageDS extends StatefulWidget {
 }
 
 class LoginPageDSState extends State<LoginPageDS> {
-  static final formKey = GlobalKey<FormState>();
+  final _formKeyLogin = GlobalKey<FormState>();
   String _userName;
   String _password;
   void validateInputsLogin() {
-    if (formKey.currentState.validate()) {
-      formKey.currentState.save();
+    if (_formKeyLogin.currentState.validate()) {
+      _formKeyLogin.currentState.save();
       print(_password);
       if (_password != null && _password.isNotEmpty) {
         widget.loginEmailPassword(
@@ -38,8 +38,8 @@ class LoginPageDSState extends State<LoginPageDS> {
   }
 
   void validateInputsResetPassword() {
-    if (formKey.currentState.validate()) {
-      formKey.currentState.save();
+    if (_formKeyLogin.currentState.validate()) {
+      _formKeyLogin.currentState.save();
       widget.sendPasswordResetEmail(_userName);
     } else {
       setState(() {});
@@ -58,7 +58,7 @@ class LoginPageDSState extends State<LoginPageDS> {
             child: Padding(
               padding: EdgeInsets.all(16.0),
               child: Form(
-                key: formKey,
+                key: _formKeyLogin,
                 child: ListView(
                   children: <Widget>[
                     Text('Informe os dados'),
